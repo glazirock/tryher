3451c5fe71dcf74e705eb0aefad398c63b2348c4
glazirock <glazirock@gmail.com>
2012-05-22 18:31:16 +0400

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tryher::Application.config.secret_token = 'dde6bb2593cc321ff965e704d9d74d6fb37ab8fcd0e9c29015e8a804af5c508bd1bfc9946a41ac26b6dca8cfaed594af0c0bb20655d71c8f289fea150545fb9f'
